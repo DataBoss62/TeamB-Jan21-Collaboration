d82d671b748db7879d1ddcde94a1d7a789e00eae
Databoss62 <hemant.mr@gmail.com>
2022-02-07 12:03:42 +0000

    created AddNumbers file

---
+++ AddNumbers.py
@@ -0,0 +1,6 @@
+print("hi Gang")
+number1 = float(input("Please enter the first number: "))
+number2 = float(input("Please enter the second number: "))
+answer = number1 + number2
+
+print(number1, "+", number2, "=", answer)
---
+++ hello2.py
@@ -0,0 +1,9 @@
+mesg = "hello world"
+print(mesg)
+print(type(mesg))
+var1 = input("Pls enter a number ")
+print(type(var1))
+var2 = int(input("Pls enter another number "))
+print(type(var2))
+var3=var2*2
+print(var3)

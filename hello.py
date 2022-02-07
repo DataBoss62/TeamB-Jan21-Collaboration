8ada9357795c54a0ac10700f4917c9891571ce28
Databoss62 <hemant.mr@gmail.com>
2022-02-07 11:37:01 +0000

    First Python codes with strings

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-my python file
+mesg = "hello world"
+print(mesg)
+print(type(mesg))
+var1 = input("Pls enter a number ")
+print(type(var1))
+var2 = int(input("Pls enter another number "))
+print(type(var2))
---
+++ helloFirstname-Lastname.py
@@ -0,0 +1,4 @@
+
+firstname = input("Hello, what is your firstname? ")
+lastname = input("Hello, what is your lastname? ")
+print ("Hello",firstname,lastname)

77fbd34cc813b26852fc18ec8caed925dec59496
Databoss62 <hemant.mr@gmail.com>
2022-02-07 16:06:20 +0000

    created hello

--- hello.py
+++ hello.py
@@ -1,7 +1,20 @@
 mesg = "hello world"
 print(mesg)
+print(mesg.upper())
+print(mesg.lower())
+mesg = mesg.split()
+print (mesg)
+mesg = "&&".join(mesg)
+print (mesg)
 print(type(mesg))
 var1 = input("Pls enter a number ")
 print(type(var1))
 var2 = int(input("Pls enter another number "))
 print(type(var2))
+
+var1 = "hello"
+var2 = "World"
+var3 = 3
+var4 = 4
+print(f"{var1} {var2}")
+print(var1 + var2)

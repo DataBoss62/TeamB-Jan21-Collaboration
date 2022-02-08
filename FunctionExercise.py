f94a310c80650a5797b6f766cd0e5546f306790e
Databoss62 <hemant.mr@gmail.com>
2022-02-08 16:27:41 +0000

    Added function exercise on overall grade

--- FunctionExercise.py
+++ FunctionExercise.py
@@ -3,16 +3,36 @@
 
 
 def Grade (homework, assessment, finalScore):
-    answer = round((((homework + assessment + finalScore)/175) * 100))
-    return answer
+    answer = (((homework + assessment + finalScore)/175) * 100)
+    return round(answer,2)
 
 
 name = input("What is your name? ")
 homework = int(input ("Enter homework score out of 25 "))
+while homework >25:
+    print("invalid number, try again")
+    homework = int(input ("Enter homework score out of 25 "))
+
 assessment = int(input("Enter assessment score out of 50 ")) 
+while assessment > 50:
+    print("invalid number, try again")
+    assessment = int(input("Enter assessment score out of 50 ")) 
+
 finalScore = int(input("Enter final score out of 100 "))
+while finalScore > 100:
+    print("invalid number, try again")
+    finalScore = int(input("Enter final score out of 100 ")) 
 
 finalGrade = Grade(homework,assessment,finalScore)
 
-print (f"{name}, Your final ICT score is {finalGrade}%" )
- 
+if finalGrade >= 80:
+    grade = "A"
+elif finalGrade >= 70:
+    grade = "B"
+elif finalGrade >= 60:
+    grade = "C"
+else:
+    grade = "F"
+
+print (f"{name}, Your final ICT score is {finalGrade}% with a grade {grade}" )
+print(f"{name} scored: {Grade(homework,assessment,finalScore)}")  # alternative
